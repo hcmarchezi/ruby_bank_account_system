c2770ef7ec9f7b611dea1dc7459d915721d6591a
Humberto Marchezi <hcmarchezi@gmail.com>
2017-01-18 21:41:36 -0200

    Refactored test to reference messages by key. Added validation for name and email format. Pending: name validation not working properly

--- domain_problem/test/models/user_test.rb
+++ domain_problem/test/models/user_test.rb
@@ -6,37 +6,55 @@ class UserTest < ActiveSupport::TestCase
     @user = FactoryGirl.build(:user)
     @user.name = ''
     assert @user.invalid? 
-    assert @user.errors.full_messages.include? "Name can't be blank"
+    assert @user.errors.full_messages.include? "Name " +  I18n.t("errors.messages.blank") 
   end
 
   test "email is not empty" do    
     @user = FactoryGirl.build(:user)
     @user.email = ''
     assert @user.invalid?
-    assert @user.errors.full_messages.include? "Email can't be blank"
+    assert @user.errors.full_messages.include? "Email " + I18n.t("errors.messages.blank")
   end
 
   test "email is unique" do
     user_1 = FactoryGirl.build(:user, email: 'abc@abc.com')
-    user_2 = FactoryGirl.build(:user, email: 'abc@abc.com')
-
     user_1.save
+    user_2 = FactoryGirl.build(:user, email: 'abc@abc.com')
     user_2.save
-
     assert user_1.valid?
     assert user_2.invalid?
-    puts user_2.errors.full_messages.to_s
-
-    assert user_2.errors.full_messages.include? "Email has already been taken"
+    assert user_2.errors.full_messages.include? "Email " + I18n.t("errors.messages.taken")
   end
 
-  # test "valid name" do
-  # end
+  test "valid name" do
+    @user = FactoryGirl.build(:user)
+    @user.name = 'Valid Name'
+    @user.save
+    assert @user.valid? 
+  end
 
-  # test "valid email" do
-  # end
+  test "invalid name" do
+    @user = FactoryGirl.build(:user)
+    @user.name = "123 Invalid &&*"
+    @user.save
+    assert @user.invalid?
+    puts "errors = " + @user.errors.full_messages.to_s
+  #   assert @user.errors.full_messages.include? "Name " + I18n.t("errors.messages.invalid")
+  end
 
-  def teardown
+  test "valid email" do
+    @user = FactoryGirl.build(:user)
+    @user.email = "valid@email.com"
+    @user.save
+    assert @user.valid? 
+  end
+
+  test "invalid email" do
+    @user = FactoryGirl.build(:user)
+    @user.email = "123@!!!!.com"
+    @user.save
+    @user.invalid?
+    assert @user.errors.full_messages.include? "Email " + I18n.t("errors.messages.invalid")
   end
 
 end
